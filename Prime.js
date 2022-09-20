d274a284a4ac6ccb0370947fbbb04bfe65ae2a0a
Santosh <santoshtajpur@gmail.com>
2022-09-20 15:00:03 +0530

    code fixed by m1

--- Prime.js
+++ Prime.js
@@ -1,7 +1,14 @@
 function checkPrime(num){
-    let bag='';
-    for(let i=0;i<num;i++){
-        bag += num(i);
+    let factor=0;
+    for(let i=2;i*i<=num;i++){
+        if(num%i===0){
+            factor++;
+        }
+        
     }
-    console.log(bag);
+    if(factor===0){
+        return true;
+    }
+    return false;
+   
 }
